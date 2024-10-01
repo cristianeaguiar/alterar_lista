101a714d6459fe43577233f7d8c5473d29d95759
cristianeaguiar <cristiane.aguiarsa@gmail.com>
2024-10-01 20:21:43 -0300

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,25 @@
+#lista de nomes
+nomes = ["Carolina", "Maria", "Eduardo", "Théo", "Sofia", "Caio"]
+
+# exibir a lista original
+for i in range(len(nomes)):
+    print(f"Índice {i}: {nomes[i]}")
+
+
+# usuário informa o índice que deseja alterar
+try:
+    indice = int(input("Informe o índice do nome que deseja alterar: "))
+    confirmacao = input(f"Deseja alterar o nome {nomes[indice]}? Digite 'sim' para confirmar.")
+
+    if confirmacao == "sim":
+        novo_nome = input("Informe o novo nome: ")
+        nomes[indice] = novo_nome
+    else:
+        ...
+except Exception as e:
+    print(f"Não foi possível alterar o nome. {e}.")
+finally:
+     # nova lista
+    for i in range(len(nomes)):
+        print(f"Índice {i}: {nomes[i]}.")
+
